44ca876c640e811468befb08de9f706d704a1463
moni <moni@DESKTOP-HJJ568L>
2017-01-16 18:50:01 +0900

    コミット

--- ServoTest/Program.cs
+++ ServoTest/Program.cs
@@ -19,49 +19,129 @@ namespace ConsoleApplication1
         }
     }
 
+    //10kHzで制御
+    public class BrushlessMotor
+    {
+        //http://toshiba.semicon-storage.com/jp/design-support/e-learning/mcupark/village/vector-1.html
+
+        //brushless motor
+        float direction = 0.0f;    //+90(CW), +70, +50, +30, +20, 0,  -20, -40, 60, -70, -90(CCW)
+
+        public void Diaptch(float power, float u, float v, float w, out float U, out float V, out float W)
+        {
+            float a, b;
+            float A, B;
+            var sinkaku = GetSinkaku(power);
+            UVWToAB(u, v, w, out a, out b);         //AB変換
+            RotateXY(a, b, sinkaku, out A, out B);  //次の進角まで回転
+            ABToUVW(A, B, out U, out V, out W);     //UVW変換
+        }
+
+        float GetSinkaku(float power)
+        {
+            if (0.0f < power)
+            {
+                //CW
+                if (+90.0f > direction)
+                {
+                    direction += 20.0f;
+                }
+            }
+            else if (0.0f > power)
+            {
+                //CCW
+                if (-90.0f < direction)
+                {
+                    direction -= 20.0f;
+                }
+            }
+            else
+            {
+                //BRAKE
+                direction = 0.0f;
+            }
+
+            return direction;
+        }
+
+        void RotateXY(float a, float b, float r, out float A, out float B)
+        {
+            var sinp = Math.Sin(r);
+            var cosp = Math.Cos(r);
+            A = (float)(a * cosp - b * sinp);
+            B = (float)(a * sinp + b * cosp);
+        }
+
+        void ABToUVW(float a, float b, out float u, out float v, out float w)
+        {
+            u = (float)Math.Sqrt(2.0 / 3.0) * a;
+            w = (float)((1.0f / Math.Sqrt(2.0/3.0)) * (-a - Math.Sqrt(3) * b));
+            v = -u - w;
+        }
+
+        void UVWToAB(float u, float v, float w, out float a, out float b)
+        {
+            a = (float)(Math.Sqrt(3.0 / 2.0) * u);
+            b = (float)(Math.Sqrt(1.0 / 2.0) * (-u - 2 * w));
+        }
+    }
+
     /// <summary>
-    /// １回転4096（内部１Tickあたり１００分解能(11bit:2048 * 4現象 * 50(ギヤ比))）
-    /// エンコーダレベルで100Tick＝軸座標１Tick
-    /// 電気レベルで4096Tick = エンコーダ256Tick
+    /// ギヤ比 1:50
+    /// 分解能 14bit(エンコーダ)
+    /// 現象   1:4
     /// 
     /// 
-    /// 電気4:エンコーダ1
-    /// エンコーダ50:実角度1
-    /// 電気200:エンコーダ50:実角度1
+    /// 設定分解能:12bit
+    /// 1pls = 200enc(エンコーダ)
+    /// 1enc = 1deg(電気角12bit)
     /// 
-    /// 電気200*4096:エンコーダ50*4096:実角度1*4096
-    /// エンコーダ1Bit削って
-    /// 電気100*4096:エンコーダ50*2048:実角度1*4096
-    /// 
+    /// まとめ
+    /// 　制御分解能(12bit, Dynamixelと同様) 単位：pls(0~4095)
+    /// 　エンコーダ分解能:14bit 単位：enc(0~16385)
+    /// 　三角関数テーブル:12bit 単位：deg(0~4095)=電気角
+    /// 　ギヤ比1:50なので、　1pls = 200enc
+    /// 　エンコーダと電気角の単位は等しい 1enc=1deg
+    /// 　
+    /// 　最小速度は、0.5ms周期で1deg
+    /// 　  1ms  = 2deg
+    /// 　  1s   = 2000deg = 2000enc = 10pls
+    /// 　  1min = 600pls
+    /// 　  600/4096rpm = 0.146484375rpm
+    /// 　
+    /// 　最大速度3000rpmの場合、
+    /// 　  3000*4096/4096rpm
+    /// 　  5*4096倍であることから
+    /// 　  0.5ms周期で 4096*5deg
     /// </summary>
     public class Servo
     {
         //サーボステータス
-        public double Position { get; private set; }    //現在位置
-        public double Speed { get; private set; }       //現在速度
-        public double Power { get; set; } = 0.0;        //現在モーター出力
+        public float Position { get; private set; }    //現在位置
+        public float Speed { get; private set; }       //現在速度
+        public float Power { get; set; } = 0.0f;        //現在モーター出力
 
         //内部パラメータ
-        double Diff { get; set; }                       //加速度補正
-        double ControlAcc { get; set; }                 //目標加速度
-        double ResultAcc { get; set; }                  //実測加速度
+        float Diff { get; set; }                       //加速度補正
+        float ControlAcc { get; set; }                 //目標加速度
+        float ResultAcc { get; set; }                  //実測加速度
 
         //設定値
-        const double LimitTorque = 50.0;                //トルク制限値
-        const double TargetSpeed = 3.0;                 //目標速度
-        const double TargetAcc = 1.0;                   //目標加速度(P)
-        const double TargetDec = 1.3;                   //目標減速度(P)
-        const double IGain = 2.0;                       //Iゲイン
-        const double DGain　= 3.0;                      //Dゲイン
+        const float LimitTorque = 50.0f;                //トルク制限値
+        const float TargetSpeed = 3.0f;                 //目標速度
+        const float TargetAcc = 1.0f;                   //目標加速度(P)
+        const float TargetDec = 1.3f;                   //目標減速度(P)
+        const float IGain = 2.0f;                       //Iゲイン
+        const float DGain　= 3.0f;                      //Dゲイン
 
         //固有特性
-        const double MotorGain = 1.2;                   //最大トルク出力値
+        const float MotorGain = 1.2f;                   //最大トルク出力値
 
         /// <summary>
         /// 位置制御
         /// </summary>
         /// <param name="goalPosition"></param>
-        public void ControlPosition(double goalPosition)
+        public void ControlPosition(float goalPosition)
         {
             var restPosition = goalPosition - Position;     //残り移動量
             ControlSpeed(restPosition);
@@ -69,40 +149,39 @@ namespace ConsoleApplication1
 
         /// <summary>
         /// 速度制御(PTP動作を実現)
-        /// X軸：時間軸、Y軸：目標速度な台形[／￣￣＼]となるような速度を調整する
+        /// X軸：時間軸、Y軸：目標速度な台形となるような速度を調整する
+        /// 
+        ///         ／￣￣＼
+        ///       ／        ＼
+        ///     ／            ＼
+        ///   ／                ＼
+        /// ／                    ＼
+        /// |  加速区間   |減速区間|
+        ///     
         /// 加速区間：目標速度に到達するように目標加速値で加速する
         /// 減速区間：目標速度が現在速度 - 目標減速値となるように減速する
         /// </summary>
         /// <param name="targetSpeed"></param>
-        public void ControlSpeed(double restPosition)
+        public void ControlSpeed(float restPosition)
         {
-            // 残りX値の求め方
-            // ピタゴラスの定理より
-            // (X+1)*Y/2 ＝ Rest
-            // (X+1)*Y   ＝ 2 * Rest
-            // Y=AX, A=DECより
-            // (X+1)*X   ＝ 2 * Rest / DEC
-            //  X^2      ≒ 2 * Rest / DEC
-            //         X ≒ √(2 * Rest / DEC)
-
             //PTP動作(時間軸Xを基準として加減速を行う)
             var rest = Math.Abs(restPosition);                      //残り移動量(絶対値)
             var hugo2 = (0 <= restPosition) ? +1.0 : -1.0;          //移動したい方向
             var MX = TargetSpeed / TargetDec;                       //減速開始位置X
-            var ZX = Math.Sqrt(2 * rest / TargetDec);               //現在位置X
+            var ZX = Math.Sqrt(2 * rest / TargetDec);               //現在位置X  (X+1)*Y/2 = Rest, Y=AX, A=Dec -> X≒√(2*Rest/Dec)
 
             if ((MX + 1) < ZX)
             {
                 //加速区間(速度制限付き、加速カーブは加速上限で表現)
                 var spd = hugo2 * Math.Min(TargetAcc, TargetSpeed - Math.Abs(Speed));
-                ControlTorque(spd);
+                ControlTorque((float)spd);
             }
             else if (TargetDec < rest)
             {
-                //減速区間(減速カーブは時系列で表現するので、目標速度にめがけてブレーキする)
+                //減速区間(1つ先の目標速度になるようブレーキをかける)
                 var targetspeed = hugo2 * (ZX - 1.0) * TargetDec;
                 var spd = targetspeed - Speed;
-                ControlTorque(spd);
+                ControlTorque((float)spd);
             }
             else
             {
@@ -117,7 +196,7 @@ namespace ConsoleApplication1
         /// 出力トルク制御(速度制御より指示された加速度を正確に実現する)
         /// </summary>
         /// <param name="acc">加速度</param>
-        public void ControlTorque(double targetAcc)
+        public void ControlTorque(float targetAcc)
         {
             ControlAcc = targetAcc;             //目標加速度（この値は補正値Diffの計算用に使用する）
             Power += targetAcc + Diff;
@@ -126,7 +205,7 @@ namespace ConsoleApplication1
             if (-LimitTorque > Power) Power = -LimitTorque;
         }
 
-        void ControlServo(int current, double power)
+        void ControlServo(int current, float power)
         {
             //TODO:サーボ制御処理はダミーコード
 
@@ -171,21 +250,21 @@ namespace ConsoleApplication1
         /// 環境負荷
         /// </summary>
         /// <returns></returns>
-        double GetFieldPower()
+        float GetFieldPower()
         {
-            var random1 = (50 - Position) / 10;
-            var random2 = -Speed / 10;
+            var random1 = (50 - Position) / 23;
+            var random2 = -Speed / 13;
             var random3 = (rnd.Next(0, 200) - 100) / 10000.0;   //エンコーダ誤差はモーター１Tickの１００分の１まで
             //random1 = random2 = 0.0;
             random3 = 0;
-            return random1 + random2 + random3;
+            return (float)(random1 + random2 + random3);
         }
 
         /// <summary>
         /// フィードバック
         /// </summary>
         /// <param name="nextPosition"></param>
-        void FeedBack(double nextPosition)
+        void FeedBack(float nextPosition)
         {
             var speed = nextPosition - Position;
             var acc = speed - Speed;
